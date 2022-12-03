e1575a974d730a764763459d1c181d195e4ade91
valverdemario <valverde1198@hotmail.com>
2022-12-03 08:35:43 -0600

    Primer commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,135 @@
+import 'dart:io';
+
+import 'package:band_names/models/band.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Band> bands = [
+    Band(id: '1', name: 'Metallica', votes: 5),
+    Band(id: '2', name: 'Queen', votes: 1),
+    Band(id: '3', name: 'Heroes del Silencio', votes: 2),
+    Band(id: '4', name: 'Bon Jovi', votes: 5),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Band Names', style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blue[300],
+        elevation: 1,
+        centerTitle: true,
+      ),
+      body: ListView.builder(
+          itemCount: bands.length,
+          itemBuilder: (context, index) => _bandTile(bands[index])),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        elevation: 1,
+        onPressed: addNewBand,
+      ),
+    );
+  }
+
+  Widget _bandTile(Band band) {
+    return Dismissible(
+      key: Key(band.id!),
+      direction: DismissDirection.startToEnd,
+      onDismissed: (direction) {
+        print('direction: $direction');
+        print('id: ${band.id}');
+      },
+      background: Container(
+        padding: const EdgeInsets.only(left: 8.0),
+        color: Color.fromARGB(255, 241, 3, 94),
+        child: const Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            'Delete Band',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(band.name!.substring(0, 2)),
+          backgroundColor: Colors.blue[100],
+        ),
+        title: Text(band.name!),
+        trailing: Text('${band.votes}', style: const TextStyle(fontSize: 20)),
+        onTap: () {
+          print(band.name);
+        },
+      ),
+    );
+  }
+
+  addNewBand() {
+    final textController = new TextEditingController();
+
+    if (Platform.isAndroid) {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: const Text("New band name"),
+              content: TextField(
+                controller: textController,
+                decoration: const InputDecoration(
+                  hintText: 'Band name',
+                ),
+              ),
+              actions: [
+                MaterialButton(
+                  child: Text("Add"),
+                  onPressed: () => addBandToList(textController.text),
+                  textColor: Colors.blue,
+                  elevation: 5,
+                )
+              ],
+            );
+          });
+    } else if (Platform.isIOS) {
+      showCupertinoDialog(
+          context: context,
+          builder: (_) {
+            return CupertinoAlertDialog(
+              title: const Text("New band name"),
+              content: CupertinoTextField(
+                controller: textController,
+              ),
+              actions: [
+                CupertinoDialogAction(
+                  isDefaultAction: true,
+                  child: const Text("Add"),
+                  onPressed: () => addBandToList(textController.text),
+                ),
+                CupertinoDialogAction(
+                  isDestructiveAction: true,
+                  child: const Text("Dismiss"),
+                  onPressed: () => Navigator.pop(context),
+                ),
+              ],
+            );
+          });
+    }
+  }
+
+  void addBandToList(String name) {
+    if (name.length > 1) {
+      // Podemos agregar
+      this
+          .bands
+          .add(new Band(id: DateTime.now().toString(), name: name, votes: 0));
+      setState(() {});
+    }
+
+    Navigator.pop(context);
+  }
+}
